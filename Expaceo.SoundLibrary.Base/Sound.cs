efc1c8000219011d8d4963bcbbb5c729b34901c9
mntdn <matmon@gmail.com>
2014-03-27 00:23:02 +0100

    Enfin un son...

--- Expaceo.SoundLibrary.Base/Sound.cs
+++ Expaceo.SoundLibrary.Base/Sound.cs
@@ -1,15 +1,167 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Media;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Expaceo.SoundLibrary.Base
 {
     public class Sound
     {
+        [DllImport("winmm.dll")]
+        public static extern int waveOutOpen(out IntPtr hWaveOut, int uDeviceID, WaveFormat lpFormat, WaveDelegate dwCallback, int dwInstance, int dwFlags);
+        [DllImport("winmm.dll")]
+        public static extern int waveOutPrepareHeader(IntPtr hWaveOut, ref WaveHdr lpWaveOutHdr, int uSize);
+        [DllImport("winmm.dll")]
+        public static extern int waveOutWrite(IntPtr hWaveOut, ref WaveHdr lpWaveOutHdr, int uSize);
+        [DllImport("winmm.dll")]
+        public static extern int waveOutUnprepareHeader(IntPtr hWaveOut, ref WaveHdr lpWaveOutHdr, int uSize);
+        [DllImport("winmm.dll")]
+        public static extern int waveOutClose(IntPtr hWaveOut);
+
+        public const int MMSYSERR_NOERROR = 0;
+        public const int CALLBACK_FUNCTION = 0x00030000;
+
+        public const int MM_WOM_OPEN = 0x3BB;
+        public const int MM_WOM_CLOSE = 0x3BC;
+        public const int MM_WOM_DONE = 0x3BD;
+
+        public static AutoResetEvent m_PlayEvent = new AutoResetEvent(false);
+        
+        // callback function
+        public delegate void WaveDelegate(IntPtr hdrvr, int uMsg, int dwUser, ref WaveHdr wavhdr, int dwParam2);
+
+        internal static void WaveOutProc(IntPtr hdrvr, int uMsg, int dwUser, ref WaveHdr wavhdr, int dwParam2)
+        {
+            Debug.WriteLine(uMsg);
+            if (uMsg == MM_WOM_DONE)
+            {
+                Debug.WriteLine("Son terminé");
+                m_PlayEvent.Set();
+            }
+        }
+        
+        private WaveDelegate m_BufferProc = new WaveDelegate(WaveOutProc);
+
+        public enum WaveFormats
+        {
+            Pcm = 1,
+            Float = 3
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class WaveFormat
+        {
+            public short wFormatTag;
+            public short nChannels;
+            public int nSamplesPerSec;
+            public int nAvgBytesPerSec;
+            public short nBlockAlign;
+            public short wBitsPerSample;
+            public short cbSize;
+
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WaveHdr
+        {
+            public IntPtr lpData; // pointer to locked data buffer
+            public int dwBufferLength; // length of data buffer
+            public int dwBytesRecorded; // used for input only
+            public IntPtr dwUser; // for client's use
+            public int dwFlags; // assorted flags (see defines)
+            public int dwLoops; // loop control counter
+            public IntPtr lpNext; // PWaveHdr, reserved for driver
+            public int reserved; // reserved for driver
+        }
+
+
+        public void PlayOKBeep(int frequency, int samplesPerSecond, short bitsPerSample)
+        {
+            IntPtr hWaveOut;
+            WaveFormat waveFormat = new WaveFormat();
+            WaveHdr WaveHeader = new WaveHdr(); 
+            int buffersize = 44100;
+
+            char[] Data = new char[buffersize]; 
+              
+            double x;
+            int i;
+              
+            waveFormat.wFormatTag = (short)WaveFormats.Pcm;
+            waveFormat.nChannels = 2;
+            waveFormat.wBitsPerSample = 8;
+            waveFormat.nSamplesPerSec = samplesPerSecond;
+            waveFormat.nBlockAlign = (short)(waveFormat.nChannels * waveFormat.wBitsPerSample / 8);
+            waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;    
+            waveFormat.cbSize = 0;
+                            
+            // ** Open the audio device **
+            if (waveOutOpen(out hWaveOut, 0, waveFormat, m_BufferProc, 0, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) 
+            {        
+                Debug.WriteLine("Sound card cannot be opened.");
+                return;
+            }
+              
+            // ** Make the sound buffer **    
+            for (i=0; i < buffersize; i++)
+            {        
+                x = Math.Sin(i * waveFormat.nChannels * Math.PI * (frequency) / (double)waveFormat.nSamplesPerSec); 
+              
+                // ** scale x to a range of 0-255 (signed char) for 8 bit sound reproduction **
+                Data[i] = (char)(127*x+128);
+            }
+              
+              
+            // ** Create the wave header for our sound buffer **
+
+            GCHandle m_HeaderDataHandle = GCHandle.Alloc(Data, GCHandleType.Pinned);
+            WaveHeader.lpData = m_HeaderDataHandle.AddrOfPinnedObject();
+            WaveHeader.dwBufferLength=buffersize;
+            WaveHeader.dwFlags=0;
+            WaveHeader.dwLoops=0;
+              
+            // ** Prepare the header for playback on sound card **
+            if (waveOutPrepareHeader(hWaveOut, ref WaveHeader, Marshal.SizeOf(WaveHeader)) != MMSYSERR_NOERROR)
+            {
+                Debug.WriteLine("Error preparing Header!");
+                return;
+            }
+              
+            // ** Play the sound! **
+            m_PlayEvent.Reset();
+
+            if (waveOutWrite(hWaveOut, ref WaveHeader, Marshal.SizeOf(WaveHeader)) != MMSYSERR_NOERROR)
+            {
+                Debug.WriteLine("Error writing to sound card!");
+                return ;
+            }
+              
+            // ** Wait until sound finishes playing
+            m_PlayEvent.WaitOne();
+              
+            // ** Unprepare our wav header **
+            if (waveOutUnprepareHeader(hWaveOut, ref WaveHeader, Marshal.SizeOf(WaveHeader)) != MMSYSERR_NOERROR)
+            {
+                Debug.WriteLine("Error unpreparing header!");
+                return;
+            }
+              
+            // ** close the wav device **
+            if (waveOutClose(hWaveOut) != MMSYSERR_NOERROR)
+            {
+                Debug.WriteLine("Sound card cannot be closed!");
+                return;
+            }
+
+            // ** Release our event handle **
+            //m_PlayEvent.Close();
+        }
 
         public static void PlayBeep(UInt16 frequency, int msDuration, UInt16 volume = 16383)
         {
--- Expaceo.SoundLibrary/MainWindow.xaml.cs
+++ Expaceo.SoundLibrary/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Expaceo.SoundLibrary.Base;
 
 namespace Expaceo.SoundLibrary
 {
@@ -20,10 +21,24 @@ namespace Expaceo.SoundLibrary
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Sound s = new Sound();
+
         public MainWindow()
         {
             InitializeComponent();
-            SoundLibrary.Base.Sound.PlayBeep(440, 1000);
+            //SoundLibrary.Base.Sound.PlayBeep(440, 1000);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int frequency;
+            int samplesPerSecond;
+            short bitsPerSample;
+            int.TryParse(TB_Frequency.Text, out frequency);
+            int.TryParse(((ListBoxItem)CB_SamplePerSec.SelectedItem).Content.ToString(), out samplesPerSecond);
+            short.TryParse(((ListBoxItem)CB_BitsPerSample.SelectedItem).Content.ToString(), out bitsPerSample);
+            s.PlayOKBeep(frequency, samplesPerSecond, bitsPerSample);
         }
     }
 }
